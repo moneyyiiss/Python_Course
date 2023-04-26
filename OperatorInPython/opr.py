ba7985ecaace6d1f881eec50fed17f5bf1731de0
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-04-26 19:54:09 +0530

    Operator in python

---
+++ OperatorInPython/opr.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+print(a + b)
+print(a - b)
+print(a * b)
+print(a/b)
+print(b%3)
+print(5**2) #5*5
+print(5**3) #5*5*5
+
+# it include
+print(10/3)
+#it eliminate digits after points
+# floor divible
+print(10//3)
