8c16676f1058d489624c967ecd12a95f66cbfcd2
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-04-27 17:01:13 +0530

    excercise

---
+++ Excercise1/python_exercise.py
@@ -0,0 +1,33 @@
+#1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+# sol-
+# break = 5
+
+# this shows invalid syntax
+
+
+# 2. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+# byear = 2001
+# cyear = 2023
+# ans = cyear - byear
+# print(ans)
+
+
+# 3. Store your first, middle and last name in three different variables and then print your full name using these variables
+
+# first = 'Manish'
+# middle = 'Mani'
+# last = 'Dinkar'
+#
+# fullname = first + " " + middle + " " + last
+# print(fullname)
+
+# 4. Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+
+# _nation = 1 valid
+# 1record = 2 invalid
+# record1 = 3 valid
+# record_one = 4 valid
+# record-one = 5 invalid
+# record^one = 6  invalid
+# continue = 7  invalid
